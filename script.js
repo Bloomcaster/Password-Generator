eabfa005be7f34b14d3a024fb433837ea21aa3b9
Tahlia <tbolden13@gmail.com>
2022-09-01 14:28:04 -0700

    fixed a curly bracket in the wrong place to run the function

--- script.js
+++ script.js
@@ -7,16 +7,18 @@ function generatePassword() {
 var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
 var numericalChar = "0123456789";
-var specialChar = "!~+_$";
+var specialChar = "!@~+_$";
 
 
 //letting user know the parameters for how many characters to use for the password
-var userCharAmount = window.prompt ("How many characters woudl you like for your password? Must be between 8 and 128 characters in length.")
+var userCharAmount = window.prompt("How many characters woudl you like for your password? Must be between 8 and 128 characters in length.")
 
 //parsing together the string that character number 
 var charNumber = parseInt(userCharAmount)
 
-if (typeInLettersEtc(userCharAmount)){
+
+//is Not a Number (isNaN)
+if(isNaN(userCharAmount)){
   window.alert("Enter a proper number between 8 and 128.")
 }
 
@@ -24,24 +26,24 @@ if (charNumber<8 || charNumber >128){
   window.alert("Enter proper number between 8 and 128.")
 }
 
+// console.log(userCharAmount)
 
-
-
+//prompting user to make choices about the types characters
 var useLowercaseChar = window.confirm("Want to use some lowercase characters?");
 var useUppercaseChar = window.confirm("Want to use some uppercase characters?");
 var useNumericalChar = window.confirm("Want to use some numerical characters?");
 var useSpecialChar = window.confirm("Want to use some special characters?");
-}
+
 
 
 //setting up the character requirements so that they must choose at least one of types of characters
-if ( 
+if( 
   useLowercaseChar === false &&
   useUppercaseChar === false &&
   useNumericalChar === false &&
   useSpecialChar === false
 ) {
-  window.alert("Must select at least on of the character types offered.")
+  window.alert("Must select at least on of the character types offered!")
 }
 
 if (useLowercaseChar) {
@@ -61,18 +63,20 @@ if (useSpecialChar){
  charChoice +=
   specialChar
 }
+// making sure that the characters are working !!! 
+// console.log(charChoice)
+
+
 
 var password = ""
 
 
-//says password length not defined on page, need to work on this!! line 45, just added those numbers, red error left page but doesnt work (maybe fixed!!)
+
 for (var i = 0; i <charNumber; i++) {
-  password += charChoice(Math.floor(Math.random()*(charChoice)));
-} 
-
-
-
-
+  password += charChoice[Math.floor(Math.random()*(charChoice.length))];
+}
+  return password;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -83,4 +87,4 @@ var passwordText = document.querySelector("#password");
 passwordText.value = password;
 }
 // Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
+generateBtn.addEventListener("click", writePassword);
